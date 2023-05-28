54fae64e8e97d819835b129849b5bc7f3eeca776
EvertonSouza12 <souza1287@live.com>
2023-05-27 23:56:48 -0300

    injecao de depedencia

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-import { Timer } from "./timer.js"
+import Timer from "./timer.js"
 import resetControls from "./controls.js"
+import Controls from "./controls.js"
 
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
@@ -12,32 +13,33 @@ const secondsDisplay = document.querySelector('.seconds')
 let minutes = Number(minutesDisplay.textContent)
 let timerTimeOut
 
+const controls = Controls({
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop
+})
+
 const timer = Timer({
   minutesDisplay,
   secondsDisplay,
   timerTimeOut,
-  resetControls
+  resetControls: controls.reset
 })
 
 buttonPlay.addEventListener('click', function() {
-  buttonPlay.classList.add('hide')
-  buttonPause.classList.remove('hide')
-  buttonSet.classList.add('hide')
-  buttonStop.classList.remove('hide')
-
+  controls.play()
   timer.countdown()
-
 })
 
 buttonPause.addEventListener('click', function() {
-  buttonPause.classList.add('hide')
-  buttonPlay.classList.remove('hide')
+  controls.pause()
   clearTimeout(timerTimeOut)
 })
 
 buttonStop.addEventListener('click', function() {
-  timer.resetControls()
-  timer.resetTimer()
+  timer.reset()
+  timer.reset()
 })
 
 buttonSoundOff.addEventListener('click', function() {
@@ -51,12 +53,13 @@ buttonSoundOn.addEventListener('click', function() {
 })
 
 buttonSet.addEventListener('click', function() {
-  let newMinutes = prompt('Quantos minutos?')
+  let newMinutes = controls.getMinutes()
+
   if (!newMinutes) {
-    timer.resetTimer()
+    timer.reset()
     return
   }
 
   minutes = newMinutes
-  updateTimerDisplay(minutes, 0)
+  timer.updateDisplay(minutes, 0)
 })
